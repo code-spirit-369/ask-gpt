9f176748e75416e9d915296238613fc67237bb99
Mindula Madhuhansa <mimmindula@gmail.com>
2024-11-12 10:53:46 +0530

    add readme

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 export const Header = () => {
   return (
     <header className="bg-background border-b p-4 flex items-center justify-between">
-      <SidebarTrigger className="block md:hidden mr-4" />
+      <SidebarTrigger className="block mr-4" />
       <Link href="/chat">
         <h1 className="text-xl font-bold">Ask-GPT</h1>
       </Link>
