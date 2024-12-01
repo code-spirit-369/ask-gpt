292d21949c97af668672ee80b6e08f6259595666
Mindula Madhuhansa <mimmindula@gmail.com>
2024-12-01 10:43:49 +0530

    add fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,7 @@ import { getStorage } from "firebase/storage";
 import { getApp, getApps, initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIAG_IMgGBAhTvv2E6fr3-rn84b7piznM",
-  authDomain: "ask-gpt-5080a.firebaseapp.com",
-  projectId: "ask-gpt-5080a",
-  storageBucket: "ask-gpt-5080a.firebasestorage.app",
-  messagingSenderId: "983920815743",
-  appId: "1:983920815743:web:17facb330d7b0ddf61194a",
+  // Add Firebase Config here
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
